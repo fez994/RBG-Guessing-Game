9b0780877ec0d12cf1a48c6c16f8381a4d45ce2a
Federico Dente <dentefederico@live.com>
2017-03-23 01:26:42 +0100

    created a function that randomly generate the rgb winner color; added a reset button, that let you reset the game once you've won. The same button also generates random colors. This makes the user free to randomly change colors whenever he/she wants.

--- js/index.js
+++ js/index.js
@@ -1,16 +1,27 @@
-var colors = [
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(0, 0, 255)",
-	"rgb(255, 0, 255)"
-];
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll('.square');
 var pickedColor = pickColor();
 var colorDisplay = document.querySelector('#colorDisplay');
 var messageDisplay = document.querySelector('#message');
+var h1 = document.querySelector('h1');
+var resetButton = document.querySelector('#reset');
+
+
+resetButton.addEventListener('click', function() {
+	// generate all new colors
+	colors = generateRandomColors(6);
+	//pick a new random color from array
+	pickedColor = pickColor();
+	//change colorDisplay to match picked Color
+	colorDisplay.textContent = pickedColor;
+	// change colors of squares
+	for(var i = 0; i < squares.length; i++) {
+		squares[i].style.background = colors[i];
+	}
+
+	h1.style.background = "#232323";
+});
 
 colorDisplay.textContent = pickedColor;
 
@@ -25,7 +36,9 @@ for(var i = 0; i < squares.length; i++) {
 		// compare color to picked color
 		if (clickedColor === pickedColor) {
 			messageDisplay.textContent = "Correct!";
+			resetButton.textContent = "Play Again?";
 			changeColors(clickedColor);
+			h1.style.background = clickedColor;
 		} else {
 			this.style.background = "#232323";
 			messageDisplay.textContent = "Try Again";
@@ -49,3 +62,29 @@ function pickColor(){
 	var random = Math.floor(Math.random() * colors.length);
 	return colors[random];
 }	
+
+
+function generateRandomColors(num) {
+	// make an array
+	var arr = [];
+	// repeat num times
+	for(var i = 0; i < num; i++){
+	//get Random color and push into arr
+		arr.push(randomColor());
+	}
+	// return that array
+	return arr;
+}
+
+
+function randomColor(){
+	// pick a "red" for 0 - 255
+	var r = Math.floor(Math.random() * 256);
+	// pick a "green" for 0 - 255
+	var g = Math.floor(Math.random() * 256);
+	// pick a "blue" for 0 - 255
+	var b = Math.floor(Math.random() * 256);
+
+	 return "rgb(" + r + ", " + g + ", " + b + ")";
+
+}
