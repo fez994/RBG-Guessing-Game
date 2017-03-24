2b1ca5ef9662968594c1ef48733e3b2688dc4b15
Federico Dente <dentefederico@live.com>
2017-03-24 14:34:11 +0100

    improved the design //  added some animation when the squares are clicked

--- js/index.js
+++ js/index.js
@@ -50,12 +50,15 @@ resetButton.addEventListener('click', function() {
 	pickedColor = pickColor();
 	//change colorDisplay to match picked Color
 	colorDisplay.textContent = pickedColor;
+	this.textContent = "New Colors";
+
+	messageDisplay.textContent = "";
 	// change colors of squares
 	for(var i = 0; i < squares.length; i++) {
 		squares[i].style.background = colors[i];
 	}
 
-	h1.style.background = "#232323";
+	h1.style.background = "steelblue";
 });
 
 colorDisplay.textContent = pickedColor;
